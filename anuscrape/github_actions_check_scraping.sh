7adef8e3c76d76296ebef6d09e19291f7c8e6a40
Obi Symons <symons.obi@gmail.com>
2019-12-20 19:47:06 +1100

    add Script

---
+++ anuscrape/github_actions_check_scraping.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+# Check if the newly scraped file does not exist. Print meaningful message and exit with failure
+if [ ! -e ./timetable.json ]; then
+    echo Scraping failed at `date` >> ./updatelog.txt
+    exit 1
+fi 
+
+# Compare the newly downloaded timetable with the previous one
+diff ./timetable.json ../data/timetable.json
+
+# If new one is different or the old one does not exist, commit new one to GitHub
+if [ $? -ne 0 ]; then
+    echo New update found at `date` >> $HOME/updatelog.txt
+    exit 0
+else 
+    echo No changes on `date` >> ./updatelog.txt
+    exit 1
+fi
